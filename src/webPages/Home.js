ae2ff6a6d708734cd2709fefd4a8ed570e50fbe6
Wesley <Gamer@DESKTOP-PQIIC82>
2020-09-13 21:16:14 -0400

    09/13/2020 Commit
    Modularized webPage content by creating content Page master folder. Updated tailwindCSS to 1.8.9

--- src/webPages/Home.js
+++ src/webPages/Home.js
@@ -1,30 +1,22 @@
 import React from 'react'
-import HeaderText from '../components/HeaderText'
+import { homeText } from './contentMaster/PageContents.js'
 
 function Home(){
     return (
         <div className="bg-gray-100 m-6">
 
-            <HeaderText name="The New You!" />
-
-            <h1 className="font-bold text-2x1">This is the home page</h1>
-                <div className="text-left p-6">
-                <div>
-                    <h3>
-                        This is demo text paragraph. Here, this paragraph of information describes your subject
-                        at the home page in greater detail.
-                    </h3>
-                    <br>
-                    </br>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum.
-                    </p>
-                </div>
-                </div>
+            <div className="text-left p-6">
+            <div>
+                <h3 className="text-center font-bold">
+                    {homeText.subHeaderH}
+                </h3>
+                <br>
+                </br>
+                <p>
+                    {homeText.mainParagraphH}
+                </p>
+            </div>
+            </div>
         </div>
     );
 };
---
+++ src/webPages/contentMaster/PageContents.js
@@ -0,0 +1,42 @@
+/**
+ * Content on every individual webpage is imported from this JS file.
+ * 
+ * Universal Page Content - Page content that can appear on any web page.
+ * Home Page Content - Page content that can appear on Home.js
+ * About Page Content - Page content that can appear on About.js
+ */
+
+/**
+ * //////////////////////////////////////////////// UNIVERSAL PAGE CONTENT //////////////////////////////////////////////////////////////
+ */
+
+let noImage = "Image has failed to load."; //Image failed to load message.
+
+/**
+ * //////////////////////////////////////////////// HOME PAGE CONTENT ///////////////////////////////////////////////////////////////////
+ */
+let homeText = {
+                subHeaderH: `Hello there! Welcome to my corner of online web development.`,
+                mainParagraphH: `Feel free to take a look around. You are currently viewing my home page. The about page contains more
+                information about me and my developer background.`
+                }; 
+
+/**
+ * //////////////////////////////////////////////// ABOUT PAGE CONTENT ///////////////////////////////////////////////////////////////////
+ */
+let aboutText = {
+                titleA: "Hello World! Meet Wesley Dzitzer",
+                mainParagraphA: `Wesley Dzitzer is a bachelor of science Managment Information Systems graduate from Rowan University
+                                since May of 2017. At Rowan University, Wesley studied object oriented programming, web design, and
+                                database design. His knowledge of programming languages include C++ and Java. In web design his
+                                knowledge includes HTML, CSS, and Java Script. In database design, Wesley is most familiar with SQL. 
+                                Wesley's previous work experience includes a PC refresh contract project and one-year technical customer 
+                                support position located at southern  New Jersey in the online iGaming casino industry. For more 
+                                information, feel free to visit the LinkedIn and GitHub profile links.`
+                };
+
+module.exports = {
+    noImage,
+    homeText,
+    aboutText
+};
